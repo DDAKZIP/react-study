bdc81e11e5926090df8a53f276e8e9ad391714e6
shkim21 <shgim9977@gmail.com>
2022-06-23 23:27:29 +0900

    배열 렌더링

--- my-app3/src/App.js
+++ my-app3/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // import Counter from './Counter';
-import InputSample from './InputSample';
+// import InputSample from './InputSample';
+import UserList from './userList';
 
 //렌더링!
 function App() {
   return (
-    <InputSample />
+    <UserList />
   );
 }
 

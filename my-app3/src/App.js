d9949feb4b7fd88a8ceae76d7043ed6873a49f85
shkim21 <shgim9977@gmail.com>
2022-06-23 13:36:57 +0900

    조건부 렌더링

--- my-app3/src/App.js
+++ my-app3/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <Wrapper>
       <Hello name="그리즐리" color="brown" isSpecial={true}/>
+      {/* <Hello name="그리즐리" color="brown" isSpecial /> */}
       <Hello name="라미" color="pink"/>
     </Wrapper>
   );
